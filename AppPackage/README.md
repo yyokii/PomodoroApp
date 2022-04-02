9f4b9dfc73b6bd0959ccbbed469cfb720f89b075
yyokii <yyokii.h@gmail.com>
2022-04-02 16:47:34 +0900

    👷 Fix project directory

--- AppPackage/README.md
+++
@@ -1,3 +0,0 @@
-# AppPackage
-
-A description of this package.
---
+++ BuildTools/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BuildTools",
+    platforms: [
+        .macOS(.v11)
+    ],
+    dependencies: [
+         .package(url: "https://github.com/apple/swift-format", from: "0.50400.0"),
+    ],
+    targets: [
+        .target(
+            name: "BuildTools",
+            // Source files for target BuildTools should be located under 'Sources/BuildTools', or a custom sources path can be set with the 'path' property in Package.swift
+            path: "")
+    ]
+)
