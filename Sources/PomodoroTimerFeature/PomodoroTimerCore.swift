3e17662d491d7b34cc6806dec909c7524226ad53
yyokii <yyokii.h@gmail.com>
2022-04-29 02:42:41 +0900

    🩹

--- Sources/PomodoroTimerFeature/PomodoroTimerCore.swift
+++ Sources/PomodoroTimerFeature/PomodoroTimerCore.swift
@@ -181,9 +181,9 @@ public let pomodoroTimerReducer = PomodoroTimerReducer { state, action, environm
             category: ["demo"],
             pomodoroState: state.pomodoroMode.mode.name
         )
-        #warning("save処理、暫定ではずしている")
+
         return .concatenate(
-//            .init(value: .saveHistory(finishedPomodoro)),
+            .init(value: .saveHistory(finishedPomodoro)),
             .init(value: .nextPomodoroMode),
             .init(value: .updatePomodoroSettings)
         )
@@ -199,7 +199,7 @@ public let pomodoroTimerReducer = PomodoroTimerReducer { state, action, environm
         }
         return .none
     case .onAppear:
-        return .init(value: .startTimer)
+        return .init(value: .updatePomodoroSettings)
     case .onDisappear:
         return .none
     }
